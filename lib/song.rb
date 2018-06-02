fb3a148861938c6c1aa5c54e6b835363d8b40ec0
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-02 21:34:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,18 +19,10 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-  # def self.reset_all
-  #   @@songs.clear
-  # end
-
+  
   def to_param
     name.downcase.gsub(' ', '-')
   end
-
-  # def self.count
-  #   self.all.count
-  # end
 
   def self.all
     @@songs
